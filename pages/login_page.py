cbe05bca710f43a23aa06bfb9edbe3891e65cf07
Anna Sokur <annasokur2605@gmail.com>
2022-11-08 19:45:42 +0100

    Add selectors

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,16 +6,15 @@ class LoginPage(BasePage):
     // *[ @ id = "login"]
     // *[contains(text(), ‘Login’ )]
     // child::div / a
-
     password_field_xpath
-    // *[ @ id =«password»]
+    // *[ @ id ="password"]
     // *[contains(text(), ‘Password’ )]
     // child::div / a
-
-    sign_in_hyperlink_xpath
+    sign_in_bottom_xpath
     // *[ @ id = "__next"] / form / div / div[2] / button / span[1]
     // *[text() =«S»ibn in]
     // child::div / a
+    email = "pati.it.qa@gmail.com"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
