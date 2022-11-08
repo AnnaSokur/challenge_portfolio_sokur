c3ef59ee2906e09c3fcc5a88731b6da248279c90
Anna Sokur <annasokur2605@gmail.com>
2022-11-08 19:51:45 +0100

    Add selectors to page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,18 +2,10 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    login_field_xpath
-    // *[ @ id = "login"]
-    // *[contains(text(), ‘Login’ )]
-    // child::div / a
-    password_field_xpath
-    // *[ @ id ="password"]
-    // *[contains(text(), ‘Password’ )]
-    // child::div / a
-    sign_in_bottom_xpath
-    // *[ @ id = "__next"] / form / div / div[2] / button / span[1]
-    // *[text() =«S»ibn in]
-    // child::div / a
+    login_field_xpath // *[ @ id = "login"]
+    password_field_xpath // *[ @ id ="password"]
+    sign_in_bottom_xpath // *[ @ id = "__next"] / form / div / div[2] / button / span[1]
+
     email = "pati.it.qa@gmail.com"
 
     def type_in_email(self, email):
