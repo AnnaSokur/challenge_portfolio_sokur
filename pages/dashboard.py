676a4515708bfcac514b0edef1294c9a104977a0
Anna Sokur <annasokur2605@gmail.com>
2022-11-08 20:32:59 +0100

    Add selectors

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,6 +2,13 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-
-
+    //label[text()=”Scounts Panel”]
+    // *[contains(text(), ‘Gracze’ )]
+    //label[text()=”Ilość graczy”]
+    // label[text() =”Ilość meczy”]
+    // *[contains(text(), ‘Ilość raportów’ )]
+    // label[text() =”Linki pomocnicze”]
+    // label[text() =”Aktywnosć”]
+    // *[ @ id = "__English"]
+    // *[ @ id = "__Wyloguj"]
     pass
--- pages/matchplayers.py
+++ pages/matchplayers.py
@@ -2,5 +2,14 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-
+    // input[contains( @ id, 'E-mail')]"
+    // input[contains( @ id, 'Imię')]"
+    //input[@placeholder ='Nazwisko' and @type = 'text']
+    //input[@placeholder ='Telefon' and @type = 'text']
+    //input[@placeholder ='Waga' or @type = 'text']
+    //input[@placeholder ='Klub' or @type = 'text']
+    // input[contains( @ id, 'Poziom rozgrywek')]"
+    // input[contains( @ id, 'Osiągnięcia')]"
+    // *[contains(text(), ‘Dodaj Link Z Youtube’ )]
+    // input[contains( @ id, 'Profil facebook')]"
     pass
